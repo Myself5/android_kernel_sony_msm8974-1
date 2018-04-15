1f958018e9ba3b8f4999f6448ab1ae327aba4dc3
Christian Oder <myself5@carbonrom.org>
2018-04-15 14:06:08 +0200

    Revert "net: wireless: bcmdhd: Fix compilation for non PCIE_FULL_DONGLE case"
    This reverts commit c9bca8880ff141be684f622387749267b6d9e9d7.

--- drivers/net/wireless/bcmdhd/dhd_linux.c
+++ drivers/net/wireless/bcmdhd/dhd_linux.c
@@ -6569,7 +6569,6 @@ dhd_clear(dhd_pub_t *dhdp)
 {
 	DHD_TRACE(("%s: Enter\n", __FUNCTION__));
 
-#ifdef PCIE_FULL_DONGLE
 	if (dhdp) {
 		int i;
 		for (i = 0; i < ARRAYSIZE(dhdp->reorder_bufs); i++) {
@@ -6587,7 +6586,6 @@ dhd_clear(dhd_pub_t *dhdp)
 		}
 		dhd_sta_pool_clear(dhdp, DHD_MAX_STA);
 	}
-#endif
 }
 
 static void
